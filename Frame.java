2768931418d5fd64fc6b764baf83b5713d96e3b0
Cipa123 <mykolas.tamonis@gmail.com>
2017-05-26 12:08:54 -0600

    Create Frame.java

---
+++ Frame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Frame  extends JFrame{
+	
+	
+
+	
+		public Frame()
+		{
+			super("Test");
+			setSize(400,400);
+			setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		}
+		
+	
+
+}
