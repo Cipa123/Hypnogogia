ba517708c12292c8bf39f38088b738bad9fa31a2
Cipa123 <mykolas.tamonis@gmail.com>
2017-05-30 20:33:54 -0600

    Update Graphic.java

--- Graphic.java
+++ Graphic.java
@@ -1,10 +1,50 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Graphic {
-
+public class Graphic implements KeyListener{
+	public 	Graphics g; 
 	public Graphic(Frame a)
 	{
-		Graphics g = a.getGraphics();
-		g.drawString("WOrk", 100, 100);
+		g = a.getGraphics();
+		g.setFont(new Font("Century Gothic", Font.PLAIN, 30));
+		g.setColor(Color.WHITE);
+		g.drawString("Hypnogogia", 450, 100);
+		a.addKeyListener(this);
+		//drawRect1();
+	}
+	public void drawRect1()
+	{
+		g.drawRect(100, 100, 100, 100);
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode() == 32)
+		{
+			g.drawString("You have pressed space", 500, 500);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode() == 32)
+		{
+			g.drawString("You have released space", 500, 600);
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+
+	
+
+
 	}
 }
