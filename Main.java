93961d3048e7430ab0df5a70c569fe4b89992058
Cipa123 <mykolas.tamonis@gmail.com>
2017-05-30 20:34:13 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,25 @@
+import java.awt.Color;
+
+
 public class Main {
+	public static Graphic g;
+	@SuppressWarnings("deprecation")
 	public static void main(String [] args)
 	{
 		Frame frame = new Frame();
 		frame.setVisible(true);
-		Graphic g = new Graphic(frame);
+		 g = new Graphic(frame);
+		 Thread animate = new Animations(frame);
+		 animate.start();
+		 		 while(frame.stopThread() < 1)
+		 {
+			 System.out.print(""+frame.stopThread());
+		 }
+		 animate.stop();
+		
+	
+		
 	}
+
+
 }
